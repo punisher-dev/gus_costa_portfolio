3212db054bce1df0aaae6b6776059bc551d831bf
Guscosta88 <guslfcosta@gmail.com>
2024-03-17 17:13:39 +1100

    Update: implementation of press to start

--- script-portfolio.js
+++ script-portfolio.js
@@ -1,6 +1,6 @@
 // Experience Accordion
-$(document).ready(function() {
-    $('.experience-titles').click(function() {
+$(document).ready(function () {
+    $('.experience-titles').click(function () {
         var accordion = $(this).data('accordion');
         $('#' + accordion).slideToggle(1000);
         $(this).parent().find('#experience-button div').toggleClass('experience-arrow-up');
@@ -8,50 +8,50 @@ $(document).ready(function() {
 });
 
 // Menu Overlay
-$(document).ready(function() {
-    $('#hamburger').click(function() {
+$(document).ready(function () {
+    $('#hamburger').click(function () {
         $('#main-menu, main').hide();
         $('.overlay').show(1000);
     });
 });
 
-$(document).ready(function() {
-    $('#back-button').click(function() {
+$(document).ready(function () {
+    $('#back-button').click(function () {
         $('.overlay').hide(500);
         $('#main-menu, main').show(1000);
     });
 });
 
-$(document).ready(function() {
-    $('#projects-overlay').click(function() {
+$(document).ready(function () {
+    $('#projects-overlay').click(function () {
         $('.overlay').hide(500);
         $('#main-menu, main').show(1000);
     });
 });
 
-$(document).ready(function() {
-    $('#experience-overlay').click(function() {
+$(document).ready(function () {
+    $('#experience-overlay').click(function () {
         $('.overlay').hide(500);
         $('#main-menu, main').show(1000);
     });
 });
 
 // different modes and high-beam sound effect
-$(document).ready(function() {
+$(document).ready(function () {
     var synthwave = new Audio('sounds/synthwave.m4a');
-    $('.mode-toggle').click(function() {
+    $('.mode-toggle').click(function () {
         var body = $('body');
         if (body.hasClass('light')) {
             body.removeClass('light').addClass('high-beam');
             synthwave.play();
             $('.sound-on').hide();
             $('.sound-off').show();
-            $('.sound-off').click(function() {
+            $('.sound-off').click(function () {
                 synthwave.pause();
                 $('.sound-off').hide();
                 $('.sound-on').show();
             });
-            $('.sound-on').click(function() {
+            $('.sound-on').click(function () {
                 synthwave.play();
                 $('.sound-on').hide();
                 $('.sound-off').show();
@@ -69,7 +69,7 @@ $(document).ready(function() {
 });
 
 // Typing effect
-$(document).ready(function() {
+$(document).ready(function () {
     var text1 = "I am gus";
     var text2 = " a designer and front end developer.";
     var textContainer1 = $("#intro");
@@ -115,7 +115,7 @@ $(document).ready(function() {
         }
     }
 
-    $('#start-button').click(function() {
+    $('#start-button, #text-cursor').click(function () {
         typeLetter1();
         $('#start-button').hide();
         $('#text-cursor').hide();
@@ -127,15 +127,15 @@ var isFlashing = true;
 
 function flash() {
     if (isFlashing) {
-        $('.intro span').fadeIn(200, function() {
-            $('.intro span').fadeOut(200, flash);
+        $('.intro span').fadeIn(600, function () {
+            $('.intro span').fadeOut(600, flash);
         });
     } else {
         $('.intro span').hide();
     }
 }
 
-$('#start-button').click(function() {
+$('#start-button, #text-cursor').click(function () {
     isFlashing = false;
 });
 
